6c8cae30320f7d72f466d567fb30ab4b4af82992
diohn <diohn_pablo@hotmail.com>
2024-10-21 01:23:15 -0300

    cors add

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,17 @@ import { errorHandling } from './middlewares/errorHandling'
 import { UPLOADS_FOLDER } from './configs/upload'
 import { schedulePaymentStatusUpdate } from "./service/SchedulePaymentStatusUpdated"
 import { createAdminUser } from "./configs/createUserAdmin"
+import cors from "cors"
 
 
 export const app = express()
 app.use(express.json())
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use("/offenders", express.static(UPLOADS_FOLDER))
 app.use(routes)
